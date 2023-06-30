eab0b357e3e564ba688c36afc3afa14358026f56
maark01 <mark.katona21@gmail.com>
2023-06-30 09:31:09 +0200

    Update PageNotFound.js

--- src/pages/PageNotFound/PageNotFound.js
+++ src/pages/PageNotFound/PageNotFound.js
@@ -7,7 +7,7 @@ const PageNotFound = () => {
             <div className="row">
                 <div className="page-not-found-col col-sm-12 col-md-12 col-lg-12">
                     <div className="page-not-found-content">
-                        <h3 className="page-not-found-header text-center">ERROR 404 - Oh NO, PAGE NOT FOUND!</h3>
+                        <h3 className="page-not-found-header text-center">ERROR 404 - OH NO, PAGE NOT FOUND!</h3>
                         <img className="page-not-found-img" src="https://www.cnet.com/a/img/resize/f812da3969350cc0b0afd5bf35ca4ffbf08cb696/hub/2019/05/22/1b710a6b-5f4d-4987-a046-c23674b221a3/picard-meme-facepalm.jpg?auto=webp&width=1200" alt="picard" />
                         <a className="beam-up-link" href={"/"}>
                             BEAM UP TO HOME!
